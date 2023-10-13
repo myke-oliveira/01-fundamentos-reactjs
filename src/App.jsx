6bd5715bb319e502225f51e320b02fa7812c5921
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com.br>
2023-10-13 16:50:56 -0300

    📓 show how properties work

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { Post } from "./Post";
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Myke Albuquerque"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio eos odit, repellat maiores libero velit explicabo. Sunt, adipisci soluta placeat quibusdam laboriosam minus totam voluptas eius iste iusto fugit maiores?"
+      />
+      <Post
+        author="Diego Fernandes"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio eos odit, repellat maiores libero velit explicabo. Sunt, adipisci soluta placeat quibusdam laboriosam minus totam voluptas eius iste iusto fugit maiores?"
+      />
     </div>
   )
 }
