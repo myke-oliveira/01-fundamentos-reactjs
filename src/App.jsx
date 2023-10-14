c8860b060950129c1a5dc1c7cfec06d8a25b78b4
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com.br>
2023-10-14 10:11:31 -0300

    🆕 feat header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post";
+import { Header } from "./componentes/Header";
+import "./global.css"
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Myke Albuquerque"
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio eos odit, repellat maiores libero velit explicabo. Sunt, adipisci soluta placeat quibusdam laboriosam minus totam voluptas eius iste iusto fugit maiores?"
