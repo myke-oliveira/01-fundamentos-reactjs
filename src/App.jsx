e96157fc710b98917182a9e583a6313b68e4e816
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com.br>
2023-10-14 11:17:31 -0300

    :new: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import { Post } from "./Post";
 import { Header } from "./componentes/Header";
+import { Sidebar } from "./componentes/Sidebar"
+
+import styles from "./App.module.css"
 import "./global.css"
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Myke Albuquerque"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio eos odit, repellat maiores libero velit explicabo. Sunt, adipisci soluta placeat quibusdam laboriosam minus totam voluptas eius iste iusto fugit maiores?"
-      />
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio eos odit, repellat maiores libero velit explicabo. Sunt, adipisci soluta placeat quibusdam laboriosam minus totam voluptas eius iste iusto fugit maiores?"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Myke Albuquerque"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio eos odit, repellat maiores libero velit explicabo. Sunt, adipisci soluta placeat quibusdam laboriosam minus totam voluptas eius iste iusto fugit maiores?"
+          />
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio eos odit, repellat maiores libero velit explicabo. Sunt, adipisci soluta placeat quibusdam laboriosam minus totam voluptas eius iste iusto fugit maiores?"
+          />
+        </main>
+      </div>
     </div>
   )
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,5 +1,4 @@
 export function Post(props) {
-  console.log(props)
   return (
     <div>
       <strong>{props.author}</strong>
